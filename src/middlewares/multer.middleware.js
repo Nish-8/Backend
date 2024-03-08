f6107ae834143f4fdd5fe85ac843890962932c77
Nish-8 <nishithd2002@gmail.com>
2024-03-08 16:51:56 +0530

    multer and cloudinary setup for handling file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp"); // where cb stands for callback function which takes two params, null (can also be error but in this case as no errors usually come) and your filepath where you want to store the file temporarily
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+//lets create a file upload method
+
+const cloudinaryFileUpload = async (localFilePath) => {
+  try {
+    //to upload file on cluodinary
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //if file has been successfully uplaoded
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log("Error while uplaodng file", error);
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { cloudinaryFileUpload };
